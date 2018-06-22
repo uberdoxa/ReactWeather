b5c8ed60ddeea699575e913d256fe04a14c218c1
ayo wynter <ayo.wynter@yahoo.com>
2018-06-21 20:31:38 -0400

    Add styles to weather page

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -9,8 +9,23 @@ var React = require('react');
 var About = (props) => {
     return (
       <div>
-      <h3>About</h3>
-      <p>Welcome to the about page</p>
+      <h1 className ='text-center'>About</h1>
+      <p>This is a weather application built on React. I have built this
+      for The Complete React Web App Developer Course.
+        </p>
+      <p>
+      Here are some of the tools I used:
+      </p>
+      <ul>
+        <li>
+        <a href="http://facebook.github.io/react">React</a> -This was
+        the javascript framework i used.
+        </li>
+          <li>
+        <a href="http://openweathermap.org/react">Open Weather Map</a> -I used
+       Open Weather Map to search for weather data by city name.
+        </li>
+      </ul>
       </div>
       );
 
